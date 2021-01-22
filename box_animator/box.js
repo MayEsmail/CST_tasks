e9d903d8a492868454c6e5bd10afda9436eafad3
May_Esmail <mayesmail12998@gmail.com>
2021-01-22 23:45:13 +0200

    moving box through the screen, JQuery

---
+++ box_animator/box.js
@@ -0,0 +1,4 @@
+$("div").animate({margin:'0 0 0 500px',height: '170px',width: '170px',},1000,function(){$(this).css('background', 'blue');});
+$("div").animate({margin:'500px 0 0 500px',height: '100px',width: '100px',},1000,function(){$(this).css('background', 'yellow');});
+$("div").animate({margin:'500px 0 0 0',height: '170px',width: '170px',},1000,function(){$(this).css('background', 'green');});
+$("div").animate({margin:'0 0 0 0',height: '100px',width: '100px',},1000,function(){$(this).css('background', 'red');});
